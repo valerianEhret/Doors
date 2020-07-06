9bdfca5ee73c0afb7c388c9a33a9928b5a0380ca
valerianEhret <vkadirovv@gmail.com>
2020-07-06 22:00:28 +0200

    commit refactoring

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -22,6 +22,10 @@ export function Navbar() {
             </div>
 
             <div className={classes.box}>
+                <NavLink  to="/door2"><img  className={classes.img}   src="https://previews.dropbox.com/p/thumb/AA2O6KVNdmnZCbqbk3j_WwiBpFw7Rag0rj6vXoG8zkPCnIIaXpC5whBhcxRd6iP8jLcBuJC9KkwUgOkBEVLEzowxkbRANWLxuRM2gh3R_2P4d6tNAI3R10M2PmE3e33y8MtwBWvUH_TxL6I4Vmx0QfkXhM63DLsIlExtCeypW1Hji6JlBe96LAurD31Xy_wmLAM68Gl13tkFq-7a1AxuZt7Ea1Pujt-0rBOctkhfKCPBpeJ7ZKfPHavivRIknqLSA46mNkNG9c6eWwqhaHuzeDjX7WTMwqPYJnVk5myQp1866r6MNgaacFwdWim7sFKEI8x0qdo3FKScVxJ4mvGMaAp3yBaC2t1pSBCSY9_4aaqy8g/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>
+            </div>
+
+            <div className={classes.box}>
                 <NavLink  to="/door3"><img  className={classes.img}   src="https://previews.dropbox.com/p/thumb/AA2O6KVNdmnZCbqbk3j_WwiBpFw7Rag0rj6vXoG8zkPCnIIaXpC5whBhcxRd6iP8jLcBuJC9KkwUgOkBEVLEzowxkbRANWLxuRM2gh3R_2P4d6tNAI3R10M2PmE3e33y8MtwBWvUH_TxL6I4Vmx0QfkXhM63DLsIlExtCeypW1Hji6JlBe96LAurD31Xy_wmLAM68Gl13tkFq-7a1AxuZt7Ea1Pujt-0rBOctkhfKCPBpeJ7ZKfPHavivRIknqLSA46mNkNG9c6eWwqhaHuzeDjX7WTMwqPYJnVk5myQp1866r6MNgaacFwdWim7sFKEI8x0qdo3FKScVxJ4mvGMaAp3yBaC2t1pSBCSY9_4aaqy8g/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>
             </div>
 
@@ -52,29 +56,6 @@ export function Navbar() {
             <div className={classes.box}>
                 <NavLink  to="/door10"><img  className={classes.img}   src="https://previews.dropbox.com/p/thumb/AA2O6KVNdmnZCbqbk3j_WwiBpFw7Rag0rj6vXoG8zkPCnIIaXpC5whBhcxRd6iP8jLcBuJC9KkwUgOkBEVLEzowxkbRANWLxuRM2gh3R_2P4d6tNAI3R10M2PmE3e33y8MtwBWvUH_TxL6I4Vmx0QfkXhM63DLsIlExtCeypW1Hji6JlBe96LAurD31Xy_wmLAM68Gl13tkFq-7a1AxuZt7Ea1Pujt-0rBOctkhfKCPBpeJ7ZKfPHavivRIknqLSA46mNkNG9c6eWwqhaHuzeDjX7WTMwqPYJnVk5myQp1866r6MNgaacFwdWim7sFKEI8x0qdo3FKScVxJ4mvGMaAp3yBaC2t1pSBCSY9_4aaqy8g/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>
             </div>
-
-
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door4"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door5"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door6"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door7"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door8"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door9"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
-            {/*<div className={classes.box}>*/}
-            {/*    <NavLink  to="/door10"><img src="https://previews.dropbox.com/p/thumb/AA3evewiCl5hZDFqz2FFN4pluNPQr5b-2-5pHYmu4KGr6xgDlwQfcl8okS6d4ZSmBpUEAaGXWaNHUzCvfKtMRRV063NWHH9LIjc3u-gX-OC79lZdMdT3Jqjlrv7jptnqxfwKB9lwqnJRC-EpMCuA3q2BSnb_PA5uF1oH75wLT5D6I_nIOnXSBATdxp3gvu3acViOO97gPrWGuaPbBffz92atZLgt41uT2lytfx9qt3A5koNBKE00jtFO-c3oPZvwImyXrRPZIQw897g6c1zvbvkmfKboytEjEEnsuWozPl_63dvLwmLYjpDgicUaNvPrpOLh2sTgoo88nKJharxycpkXF7bSvwT6r__EP2C32jM-eg/p.png?fv_content=true&size_mode=5" alt="door"/></NavLink>*/}
-            {/*</div>*/}
 
         </div>
     )
